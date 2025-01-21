b423d8ca2c0d16a7b24a8f0c80888ab7945aee42
Cassie Cheung <hwc39103@uowmail.edu.au>
2025-01-21 11:25:42 +0800

    chore: reinit for term b

---
+++ AST21105/Week1/H10007740_Q1.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string name;
+	cout << "Hi! What is your name? ";
+	cin >> name;
+	cout << "Hello, " << name << "!" << endl;
+
+	return 0;
+}
